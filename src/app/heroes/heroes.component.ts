b6338315bb830c194d93664a9f42c56b8fcd1743
LAPTOP-B57F2LVU\BU <takeshita-masato527@gmail.com>
2018-11-17 14:00:19 +0900

    1.ヒーローエディター完了

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+
   constructor() { }
 
   ngOnInit() {
